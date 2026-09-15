85e5031093beea07d7e16de6b5343d75ce57304c
Kaiser3912 <zed864213@gmail.com>
2026-09-15 16:23:39 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtHoTen = new TextBox();
+            txtNamSinh = new TextBox();
+            txtEmail = new TextBox();
+            grbGioiTinh = new GroupBox();
+            rdoNam = new RadioButton();
+            rdoNu = new RadioButton();
+            cboKhoa = new ComboBox();
+            btnHienThi = new Button();
+            btnXoa = new Button();
+            btnThoat = new Button();
+            label6 = new Label();
+            grbGioiTinh.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 122);
+            label1.Margin = new Padding(4, 0, 3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Họ và Tên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Năm sinh";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(42, 211);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(42, 272);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Giới tính";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(42, 322);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Lớp/Khoa";
+            // 
+            // txtHoTen
+            // 
+            txtHoTen.Location = new Point(145, 119);
+            txtHoTen.Name = "txtHoTen";
+            txtHoTen.Size = new Size(295, 27);
+            txtHoTen.TabIndex = 5;
+            // 
+            // txtNamSinh
+            // 
+            txtNamSinh.Location = new Point(145, 165);
+            txtNamSinh.Name = "txtNamSinh";
+            txtNamSinh.Size = new Size(295, 27);
+            txtNamSinh.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(145, 211);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(295, 27);
+            txtEmail.TabIndex = 7;
+            // 
+            // grbGioiTinh
+            // 
+            grbGioiTinh.Controls.Add(rdoNam);
+            grbGioiTinh.Controls.Add(rdoNu);
+            grbGioiTinh.Location = new Point(145, 244);
+            grbGioiTinh.Name = "grbGioiTinh";
+            grbGioiTinh.Size = new Size(295, 57);
+            grbGioiTinh.TabIndex = 8;
+            grbGioiTinh.TabStop = false;
+            // 
+            // rdoNam
+            // 
+            rdoNam.AutoSize = true;
+            rdoNam.Location = new Point(124, 26);
+            rdoNam.Name = "rdoNam";
+            rdoNam.Size = new Size(62, 24);
+            rdoNam.TabIndex = 9;
+            rdoNam.TabStop = true;
+            rdoNam.Text = "Nam";
+            rdoNam.UseVisualStyleBackColor = true;
+            // 
+            // rdoNu
+            // 
+            rdoNu.AutoSize = true;
+            rdoNu.Location = new Point(0, 26);
+            rdoNu.Name = "rdoNu";
+            rdoNu.Size = new Size(50, 24);
+            rdoNu.TabIndex = 10;
+            rdoNu.TabStop = true;
+            rdoNu.Text = "Nữ";
+            rdoNu.UseVisualStyleBackColor = true;
+            // 
+            // cboKhoa
+            // 
+            cboKhoa.FormattingEnabled = true;
+            cboKhoa.Location = new Point(145, 322);
+            cboKhoa.Name = "cboKhoa";
+            cboKhoa.Size = new Size(151, 28);
+            cboKhoa.TabIndex = 11;
+            // 
+            // btnHienThi
+            // 
+            btnHienThi.Location = new Point(117, 392);
+            btnHienThi.Name = "btnHienThi";
+            btnHienThi.Size = new Size(94, 29);
+            btnHienThi.TabIndex = 12;
+            btnHienThi.Text = "Hiển thị";
+            btnHienThi.UseVisualStyleBackColor = true;
+            btnHienThi.Click += btnHienThi_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(279, 392);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 13;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(455, 392);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(94, 29);
+            btnThoat.TabIndex = 14;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F);
+            label6.Location = new Point(145, 40);
+            label6.Name = "label6";
+            label6.Size = new Size(322, 41);
+            label6.TabIndex = 15;
+            label6.Text = "THÔNG TIN SINH VIÊN";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(641, 500);
+            Controls.Add(label6);
+            Controls.Add(btnThoat);
+            Controls.Add(btnXoa);
+            Controls.Add(btnHienThi);
+            Controls.Add(cboKhoa);
+            Controls.Add(grbGioiTinh);
+            Controls.Add(txtEmail);
+            Controls.Add(txtNamSinh);
+            Controls.Add(txtHoTen);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            grbGioiTinh.ResumeLayout(false);
+            grbGioiTinh.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtHoTen;
+        private TextBox txtNamSinh;
+        private TextBox txtEmail;
+        private GroupBox grbGioiTinh;
+        private RadioButton rdoNam;
+        private RadioButton rdoNu;
+        private ComboBox cboKhoa;
+        private Button btnHienThi;
+        private Button btnXoa;
+        private Button btnThoat;
+        private Label label6;
+    }
+}
